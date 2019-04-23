8a0ad6219fbce4415d11085c3906f2f6a3b63f9f
Tigran-teq-Tadevosyan <tigran@tadevosyan.net>
2019-04-23 13:14:13 +0400

    Fix

--- auth.js
+++ auth.js
@@ -8,7 +8,6 @@ module.exports = (req, res, next) => {
     //     username:'Tigran'
     // }
     if (!req.cookies.userdata) return next();
-    
     User.findOne({_id:req.cookies.userdata},(err,user)=>{
         if(err || !user) return next();
         req.user = user;
