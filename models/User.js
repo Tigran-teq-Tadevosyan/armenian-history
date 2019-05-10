3b057001b803d6b3c20a85691e913b5f8d94c9e3
Tigran-teq-Tadevosyan <tigran@tadevosyan.net>
2019-05-10 06:53:21 +0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 var UserSchema = new Schema({
     username: { type: String, required: true, index: { unique: true } },
     password: String,
-    email:  { type: String, required: true, index: { unique: true } },
+    email: type: String,
     reviews: Array
 });
 var User = mongoose.model('User', UserSchema);
